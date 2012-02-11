8b5d62985446f69c2234ad17d8b6a9185e4ed095
Omar Vargas <ovargas27@gmail.com>
2012-02-10 19:24:42 -0700

    Devise added

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,9 @@ module RolesManagement
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # forcing to not access the DB or load models when precompiling your assets
+    config.assets.initialize_on_precompile = false
+
   end
 end
