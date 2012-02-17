3375d9e644d28c8c7c4af53834e0cfb6fa864dc7
Omar Vargas <ovargas27@gmail.com>
2012-02-17 10:22:51 -0700

    specs fixed

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,4 +1,7 @@
 RSpec.configure do |config|
   config.include Devise::TestHelpers, :type => :controller
   config.extend ControllerMacros, :type => :controller
+
+  config.include Devise::TestHelpers, :type => :view
+  config.extend ControllerMacros, :type => :view
 end
--- spec/views/posts/index.html.haml_spec.rb
+++ spec/views/posts/index.html.haml_spec.rb
@@ -14,6 +14,8 @@ describe "posts/index" do
     ])
   end
 
+  login_user
+
   it "renders a list of posts" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
